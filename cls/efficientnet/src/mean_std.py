863154617b7793475f50b357f09bea87a37f47fd
由博文 <6527625+from_bowen@user.noreply.gitee.com>
2020-08-16 22:55:19 +0800

    asd

--- cls/efficientnet/src/mean_std.py
+++ cls/efficientnet/src/mean_std.py
@@ -13,25 +13,22 @@ def calc_mean_std():
     mean = np.array([0.4914, 0.4822, 0.4465])
     std = np.array([0.2470, 0.2435, 0.2616])
     print('default mean & std: \n', mean, std)
-    means = [0, 0, 0]
+    mean = [0, 0, 0]
     std = [0, 0, 0]
 
-    index = 1
-    num_imgs = 0
     for each in tqdm(path):
-        num_imgs += 1
         img = cv2.imread(each).astype(np.float32) / 255.
         for i in range(3):
-            means[i] += img[:, :, i].mean()
-            stdevs[i] += img[:, :, i].std()
+            mean[i] += img[:, :, i].mean()
+            std[i] += img[:, :, i].std()
 
-    means.reverse()
+    mean.reverse()
     std.reverse()
 
-    means = np.asarray(means) / num_imgs
-    std = np.asarray(stdevs) / num_imgs
+    mean = np.asarray(mean) / len(path)
+    std = np.asarray(std) / len(path)
     print('our dataset: \n', mean, std)
-    return means,std
+    return mean,std
 
 
 
--- cls/efficientnet/src/utils/config.py
+++ cls/efficientnet/src/utils/config.py
@@ -93,7 +93,7 @@ LR = 1e-3
 # 下面的参数一般要改
 
 num_classes = 20
-root = './' # 传给data_pps的参数
+root = '/content/dataset' # 传给data_pps的参数
 epochs = 20
 model_path = '' # 预训练模型的位置
 outdir = './' # 路径后面不能有斜杠   '%s/net_%03d_%.3f.pth' % (config.outdir, epoch + 1,acc))
