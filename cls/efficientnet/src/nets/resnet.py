168d120f588ae1175a5676a7048f6a4aed044ab8
由博文 <6527625+from_bowen@user.noreply.gitee.com>
2020-08-29 17:56:36 +0800

    asd

--- cls/efficientnet/src/nets/resnet.py
+++ cls/efficientnet/src/nets/resnet.py
@@ -295,7 +295,19 @@ def resnext50_32x4d(pretrained=False, progress=True, **kwargs):
     kwargs['width_per_group'] = 4
     return _resnet('resnext50_32x4d', Bottleneck, [3, 4, 6, 3],
                    pretrained, progress, **kwargs)
-
+                   
+                   
+def resnext101_32x8d(pretrained=False, progress=True, **kwargs):
+    r"""ResNeXt-101 32x8d model from
+    `"Aggregated Residual Transformation for Deep Neural Networks" <https://arxiv.org/pdf/1611.05431.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    kwargs['groups'] = 32
+    kwargs['width_per_group'] = 8
+    return _resnet('resnext101_32x8d', Bottleneck, [3, 4, 23, 3],
+                   pretrained, progress, **kwargs)
 
 def wide_resnet50_2(pretrained=False, progress=True, **kwargs):
 
