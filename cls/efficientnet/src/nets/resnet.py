10bd16a95ed201536c2e58a90b1bb48747af4f61
由博文 <6527625+from_bowen@user.noreply.gitee.com>
2020-08-29 19:14:32 +0800

    asd

--- cls/efficientnet/src/nets/resnet.py
+++ cls/efficientnet/src/nets/resnet.py
@@ -274,7 +274,10 @@ class ResNet(nn.Module):
 
 def _resnet(arch, block, layers, pretrained, progress, **kwargs):
     model = ResNet(block, layers, **kwargs)
-    if pretrained:
+    if 'pre_model' in kwargs.keys():
+        state_dict = torch.load(kwargs['pre_model'])
+        model.load_state_dict(state_dict)
+    elif pretrained:
         state_dict = load_state_dict_from_url(model_urls[arch],
                                               progress=progress)
         model.load_state_dict(state_dict)
@@ -288,7 +291,6 @@ def resnet50(pretrained=False, progress=True, **kwargs):
                    **kwargs)
 
 
-
 def resnext50_32x4d(pretrained=False, progress=True, **kwargs):
 
     kwargs['groups'] = 32
@@ -298,12 +300,7 @@ def resnext50_32x4d(pretrained=False, progress=True, **kwargs):
                    
                    
 def resnext101_32x8d(pretrained=False, progress=True, **kwargs):
-    r"""ResNeXt-101 32x8d model from
-    `"Aggregated Residual Transformation for Deep Neural Networks" <https://arxiv.org/pdf/1611.05431.pdf>`_
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
+
     kwargs['groups'] = 32
     kwargs['width_per_group'] = 8
     return _resnet('resnext101_32x8d', Bottleneck, [3, 4, 23, 3],
@@ -317,16 +314,7 @@ def wide_resnet50_2(pretrained=False, progress=True, **kwargs):
 
 
 def wide_resnet101_2(pretrained=False, progress=True, **kwargs):
-    r"""Wide ResNet-101-2 model from
-    `"Wide Residual Networks" <https://arxiv.org/pdf/1605.07146.pdf>`_
-    The model is the same as ResNet except for the bottleneck number of channels
-    which is twice larger in every block. The number of channels in outer 1x1
-    convolutions is the same, e.g. last block in ResNet-50 has 2048-512-2048
-    channels, and in Wide ResNet-50-2 has 2048-1024-2048.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
+
     kwargs['width_per_group'] = 64 * 2
     return _resnet('wide_resnet101_2', Bottleneck, [3, 4, 23, 3],
                    pretrained, progress, **kwargs)
